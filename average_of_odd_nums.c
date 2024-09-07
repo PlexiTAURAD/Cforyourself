0720f15655c7662b9149029cf07d62e779cc23b2
Arnav Upadhyay <73123210+PlexiTAURAD@users.noreply.github.com>
2024-09-07 23:23:07 +0530

    while loops and increments

---
+++ average_of_odd_nums.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(void){
+    int i,r,c,t;
+    float a;
+    printf("Enter the range: ");
+    scanf("%d",&r);
+    i=1;
+    c=0;
+    t=0;
+    while(i<r)
+    {
+        if(i%2!=0)
+        {
+            t=t+i;
+            c++;
+        }
+        i++;
+    }
+    a = t/c;
+    printf("\nThe average of odd numbers below %d: %f",r,a);
+return 0;
+}
