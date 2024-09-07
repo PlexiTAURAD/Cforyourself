1ebff31269552808019847acd240d6ed1977c21f
Arnav Upadhyay <73123210+PlexiTAURAD@users.noreply.github.com>
2024-09-07 21:46:54 +0530

    defining variables and scanf

---
+++ area_and_perimeter_of_square_and_rectangle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void){
+    int a,l,b;
+    float pi=3.14;
+    printf("Enter the side of your square: ");
+    scanf("%d", &a);
+    printf("\nThe area of your square is: %d", a*a);
+    printf("\nThe perimeter of your square: %d", 4*a);
+    printf("\nEnter length and breadth of your rectangle: ");
+    scanf("%d%d", &l,&b);
+    printf("\nThe area of your rectangle is: %d", l*b);
+    printf("\nThe perimeter of your rectangle is: %d", 2*(l+b));
+}
